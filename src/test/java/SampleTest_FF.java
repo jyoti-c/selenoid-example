853180532c20115cc3543d7458e98a7f5536d719
jyoti.chabria <jyoti.chabria@logicmonitor.com>
2023-03-22 17:41:27 +0530

    Added suite for parallel execution with firefox and chrome

--- src/test/java/SampleTest_FF.java
+++ src/test/java/SampleTest_FF.java
@@ -1,14 +1,11 @@
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.remote.SessionId;
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -16,13 +13,13 @@ import org.testng.annotations.Test;
 /**
  * Created By jyotichabria on 26/10/22
  **/
-public class SampleTest {
+public class SampleTest_FF {
 
     WebDriver webDriver;
 
     @BeforeTest
     public void setUp() throws MalformedURLException {
-        ChromeOptions options = new ChromeOptions();
+        FirefoxOptions options = new FirefoxOptions();
         options.setCapability("enableVNC",true);
         options.setCapability("enableVideo",true);
         webDriver=new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), options);
@@ -42,7 +39,7 @@ public class SampleTest {
             Assert.assertEquals(webDriver.getTitle(), "YouTube", "Title does not match");
             Thread.sleep(1000);
             webDriver.navigate().to("https://www.facebook.com");
-            Assert.assertEquals(webDriver.getTitle(), "Meta", "Title does not match");
+            Assert.assertNotEquals(webDriver.getTitle(), "Meta", "Title does not match");
 
         }
         catch (Exception e) {
